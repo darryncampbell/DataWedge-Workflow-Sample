c7b7cc0e2dcf36c43344f909c62f43a5bd0b8b72
darryncampbell <darryncampbell@hotmail.com>
2022-02-04 09:15:31 +0000

    OCR testing and impl

--- app/src/main/java/com/darryncampbell/datawedgeworkflowsample/MainActivity.java
+++ app/src/main/java/com/darryncampbell/datawedgeworkflowsample/MainActivity.java
@@ -33,10 +33,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DWUtilities.CreateDWProfile(this);
+        //DWUtilities.CreateDWProfile(this);
         DWUtilities.registerForNotifications(this, DWUtilities.NOTIFICATION_TYPE_SCANNER_STATUS);
         DWUtilities.registerForNotifications(this, DWUtilities.NOTIFICATION_TYPE_WORKFLOW_STATUS);
 
+        Intent initiatingIntent = getIntent();
+        if (initiatingIntent != null)
+        {
+            String action = initiatingIntent.getAction();
+            if (action.equalsIgnoreCase(getResources().getString(R.string.activity_intent_filter_action)))
+            {
+                //  Received a barcode through StartActivity
+                processScan(getApplicationContext(), initiatingIntent);
+            }
+        }
     }
 
     @Override
@@ -49,11 +59,12 @@ public class MainActivity extends AppCompatActivity {
         filter.addAction(DWUtilities.NOTIFICATION_ACTION);
         registerReceiver(myBroadcastReceiver, filter);
 
-        updateTextView(R.id.txtScannerStatus, "Waiting for Status");
-        updateTextView(R.id.txtWorkflowStatus, "Waiting for Status");
-        updateTextView(R.id.txtWorkflowResult, "Please Scan Barcode");
-        updateTextView(R.id.txtDataString, "Please Scan Barcode");
-        updateImage(R.id.img, null);
+        //  todo reinstate
+        //updateTextView(R.id.txtScannerStatus, "Waiting for Status");
+        //updateTextView(R.id.txtWorkflowStatus, "Waiting for Status");
+        //updateTextView(R.id.txtWorkflowResult, "Please Scan Barcode");
+        //updateTextView(R.id.txtDataString, "Please Scan Barcode");
+        //updateImage(R.id.img, null);
     }
 
     @Override
@@ -64,117 +75,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private BroadcastReceiver myBroadcastReceiver = new BroadcastReceiver() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(getResources().getString(R.string.activity_intent_filter_action)))
-            {
-                updateTextView(R.id.txtDataString, "Please Scan Barcode");
-                updateTextView(R.id.txtWorkflowResult, "Please Scan Barcode");
-
-                //  Note: Intent Output plugin setting for 'Use Content providers' is not used for Workflow or barcode highlighting
-                updateTextView(R.id.txtTime, dateFormat.format(new Timestamp(System.currentTimeMillis())));
-
-                String source = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_source));
-                Log.d(LOG_TAG, "Decode Source: " + source);
-                updateTextView(R.id.txtSource, source);
-
-                String dataString = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_data_string));
-                Log.d(LOG_TAG, "Decode Data String: " + dataString);
-                updateTextView(R.id.txtDataString, dataString);
-
-                String decodedMode = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_decoded_mode));
-                Log.d(LOG_TAG, "Decode Mode: " + decodedMode);
-                updateTextView(R.id.txtDecodeMode, decodedMode);
-
-                String symbology = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_label_type));
-                Log.d(LOG_TAG, "Decode Symbology: " + symbology);
-                updateTextView(R.id.txtSymbology, symbology);
-
-                String data = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_data));
-                Log.d(LOG_TAG, "Decode Data: " + data);
-                if (data != null)
-                {
-                    try {
-                        JSONArray dataArray = new JSONArray(data);
-                        updateTextView(R.id.txtWorkflowResult, dataArray.toString(2));
-                        for (int i = 0; i < dataArray.length(); i++)
-                        {
-                            JSONObject workflowObject = dataArray.getJSONObject(i);
-                            Log.v(LOG_TAG, "Workflow Object: " + workflowObject.toString());
-                            if (workflowObject.has("string_data"))
-                            {
-                                Log.d(LOG_TAG, "Workflow " + i + " string_data: " + workflowObject.get("string_data"));
-                                String label = workflowObject.getString("label");
-                                if (label.equalsIgnoreCase("License Plate"))
-                                {
-                                    Log.d(LOG_TAG, "License Plate");
-                                    //  todo test this
-                                    updateTextView(R.id.txtOCRResult, "License Plate: " + workflowObject.get("string_data"));
-                                    updateTextView(R.id.lblImage, "License Plate Image");
-                                }
-                                else if (label.equalsIgnoreCase("Identification Document"))
-                                {
-                                    Log.d(LOG_TAG, "Identification Document");
-                                    //  todo parse identity document (https://techdocs.zebra.com/datawedge/11-2/guide/programmers-guides/workflow-input/#ocrresult)
-                                    //updateTextView(R.id.txtOCRResult, "ID: " + workflowObject.get("string_data"));
-                                    updateTextView(R.id.lblImage, "Identity Document Image");
-                                }
-                                else if (label.equalsIgnoreCase("VIN"))
-                                {
-                                    Log.d(LOG_TAG, "VIN (Vehicle Identification Number)");
-                                    //  todo test this
-                                    updateTextView(R.id.txtOCRResult, "Vehicle ID: " + workflowObject.get("string_data"));
-                                    updateTextView(R.id.lblImage, "Vehicle ID Image");
-                                }
-                                else if (label.equalsIgnoreCase("TIN"))
-                                {
-                                    Log.d(LOG_TAG, "TIN (Tyre Identification Number)");
-                                    //  todo test this
-                                    updateTextView(R.id.txtOCRResult, "Tyre ID: " + workflowObject.get("string_data"));
-                                    updateTextView(R.id.lblImage, "Tyre ID Image");
-                                }
-                                else if (label.equalsIgnoreCase("Meter"))
-                                {
-                                    Log.d(LOG_TAG, "Meter (e.g. Gas meter)");
-                                    //  todo test this
-                                    updateTextView(R.id.txtOCRResult, "Meter: " + workflowObject.get("string_data"));
-                                    updateTextView(R.id.lblImage, "Meter Image");
-                                }
-                                else{
-                                    Log.d(LOG_TAG, "Freeform image capture");
-                                    updateTextView(R.id.lblImage, "Freeform Image Capture Image");
-                                }
-                            }
-                            else
-                            {
-                                //  Image data
-                                if (workflowObject.has("uri")) {
-                                    String uriAsString = workflowObject.get("uri").toString();
-                                    Log.d(LOG_TAG, "Workflow " + i + " uri: " + uriAsString);
-                                    byte[] imageAsBytes = null;
-                                    if (uriAsString != null)
-                                        imageAsBytes = processUri(uriAsString);
-                                    int width = workflowObject.getInt("width");
-                                    int height = workflowObject.getInt("height");
-                                    int stride = workflowObject.getInt("stride");
-                                    int orientation = workflowObject.getInt("orientation");
-                                    String imageFormat = workflowObject.getString("imageformat");
-
-                                    Bitmap bitmap = null;
-                                    if (imageAsBytes != null)
-                                    {
-                                        bitmap = ImageProcessing.getInstance().getBitmap(imageAsBytes, imageFormat, orientation, stride, width, height);
-                                        updateImage(R.id.img, bitmap);
-                                    }else
-                                        Log.w(LOG_TAG, "Error processing Image data");
-                                }
-                            }
-                        }
-                    } catch (JSONException e) {
-                        Log.w(LOG_TAG, "JSON Exception parsing data: " + e.getMessage());
-                    }
-                }
+            if (intent.getAction().equals(getResources().getString(R.string.activity_intent_filter_action))) {
+                processScan(context, intent);
             }
             else if (intent.getAction().equals(DWUtilities.NOTIFICATION_ACTION))
             {
@@ -197,6 +101,137 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+    private void processScan(Context context, Intent intent)
+    {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        if (intent.getAction().equals(getResources().getString(R.string.activity_intent_filter_action)))
+        {
+            updateTextView(R.id.txtDataString, "Please Scan Barcode");
+            updateTextView(R.id.txtWorkflowResult, "Please Scan Barcode");
+
+            //  Note: Intent Output plugin setting for 'Use Content providers' is not used for Workflow or barcode highlighting
+            updateTextView(R.id.txtTime, dateFormat.format(new Timestamp(System.currentTimeMillis())));
+
+            String source = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_source));
+            Log.d(LOG_TAG, "Decode Source: " + source);
+            updateTextView(R.id.txtSource, source);
+
+            String dataString = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_data_string));
+            Log.d(LOG_TAG, "Decode Data String: " + dataString);
+            updateTextView(R.id.txtDataString, dataString);
+
+            String decodedMode = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_decoded_mode));
+            Log.d(LOG_TAG, "Decode Mode: " + decodedMode);
+            updateTextView(R.id.txtDecodeMode, decodedMode);
+
+            String symbology = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_label_type));
+            Log.d(LOG_TAG, "Decode Symbology: " + symbology);
+            updateTextView(R.id.txtSymbology, symbology);
+
+            String data = intent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_data));
+            Log.d(LOG_TAG, "Decode Data: " + data);
+            if (data != null)
+            {
+                try {
+                    JSONArray dataArray = new JSONArray(data);
+                    updateTextView(R.id.txtWorkflowResult, dataArray.toString(2));
+                    for (int i = 0; i < dataArray.length(); i++)
+                    {
+                        JSONObject workflowObject = dataArray.getJSONObject(i);
+                        Log.v(LOG_TAG, "Workflow Object: " + workflowObject.toString());
+                        if (workflowObject.has("string_data"))
+                        {
+                            Log.d(LOG_TAG, "Workflow " + i + " string_data: " + workflowObject.get("string_data"));
+                            String label = workflowObject.getString("label");
+                            if (label.equalsIgnoreCase("lastName"))
+                            {
+                                parseIdentityDocument(dataArray);
+                                break;
+                            }
+                            else if (label.equalsIgnoreCase("License Plate Number"))
+                            {
+                                Log.d(LOG_TAG, "License Plate");
+                                updateTextView(R.id.txtOCRResult, "License Plate: " + workflowObject.get("string_data"));
+                                updateTextView(R.id.lblImage, "License Plate Image");
+                            }
+                            else if (label.equalsIgnoreCase("VIN Number"))
+                            {
+                                Log.d(LOG_TAG, "VIN (Vehicle Identification Number)");
+                                updateTextView(R.id.txtOCRResult, "Vehicle ID: " + workflowObject.get("string_data"));
+                                updateTextView(R.id.lblImage, "Vehicle ID Image");
+                            }
+                            else if (label.equalsIgnoreCase("TIN Number"))
+                            {
+                                Log.d(LOG_TAG, "TIN (Tyre Identification Number)");
+                                updateTextView(R.id.txtOCRResult, "Tyre ID: " + workflowObject.get("string_data"));
+                                updateTextView(R.id.lblImage, "Tyre ID Image");
+                            }
+                            else if (label.equalsIgnoreCase("Meter Reading"))
+                            {
+                                Log.d(LOG_TAG, "Meter (e.g. Gas meter)");
+                                updateTextView(R.id.txtOCRResult, "Meter: " + workflowObject.get("string_data"));
+                                updateTextView(R.id.lblImage, "Meter Image");
+                            }
+                            else{
+                                Log.d(LOG_TAG, "Freeform image capture");
+                                updateTextView(R.id.lblImage, "Freeform Image Capture Image");
+                            }
+                        }
+                        else
+                        {
+                            //  Image data
+                            if (workflowObject.has("uri")) {
+                                String uriAsString = workflowObject.get("uri").toString();
+                                Log.d(LOG_TAG, "Workflow " + i + " uri: " + uriAsString);
+                                byte[] imageAsBytes = null;
+                                if (uriAsString != null)
+                                    imageAsBytes = processUri(uriAsString);
+                                int width = workflowObject.getInt("width");
+                                int height = workflowObject.getInt("height");
+                                int stride = workflowObject.getInt("stride");
+                                int orientation = workflowObject.getInt("orientation");
+                                String imageFormat = workflowObject.getString("imageformat");
+
+                                Bitmap bitmap = null;
+                                if (imageAsBytes != null)
+                                {
+                                    bitmap = ImageProcessing.getInstance().getBitmap(imageAsBytes, imageFormat, orientation, stride, width, height);
+                                    updateImage(R.id.img, bitmap);
+                                }else
+                                    Log.w(LOG_TAG, "Error processing Image data");
+                            }
+                        }
+                    }
+                } catch (JSONException e) {
+                    Log.w(LOG_TAG, "JSON Exception parsing data: " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    private void parseIdentityDocument(JSONArray dataArray) throws JSONException {
+        Log.d(LOG_TAG, "Identification Document");
+
+        String lastName = "?";
+        String firstName = "?";
+        //  parse identity document (https://techdocs.zebra.com/datawedge/11-2/guide/programmers-guides/workflow-input/#ocrresult)
+        for (int i = 0; i < dataArray.length(); i++)
+        {
+            JSONObject workflowObject = dataArray.getJSONObject(i);
+            Log.v(LOG_TAG, "Workflow Object: " + workflowObject.toString());
+            if (workflowObject.has("string_data")) {
+                String label = workflowObject.getString("label");
+                if (label.equalsIgnoreCase("lastName")) {
+                    lastName = workflowObject.getString("string_data");
+                }
+                else if (label.equalsIgnoreCase("firstName"))
+                    firstName = workflowObject.getString("string_data");
+            }
+        }
+        updateTextView(R.id.txtOCRResult, "First Name: " + firstName + ", Last Name: " + lastName);
+        updateTextView(R.id.lblImage, "Identity Document Image");
+    }
 
     private void updateTextView(int textViewId, String data)
     {
